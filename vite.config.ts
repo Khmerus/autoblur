950d7b293ac0270c51a99d43c3c3a32dbe1a172f
Khmerus <2813244@gmail.com>
2026-01-05 00:08:52 +0500

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   define: {
-    // Это позволит коду использовать process.env.API_KEY в браузере
     'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
   }
 });
